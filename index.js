1ba478635fbb8b5eea027f6916384625a821e0cc
Derek Silva <dereksilvamusic@gmail.com>
2018-05-04 22:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function theBeatlesPlay(musicians, instruments) {
   var emptyArray = []
   for (var i = 0; i < musicians.length; i++) { 
   emptyArray.push(musicians[0] + 'plays' + instruments[0])
-   
+  
 }}
