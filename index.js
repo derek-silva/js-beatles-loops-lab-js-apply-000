e54a7002e38ba0c99eca696e7210bed56f26e52d
Derek Silva <dereksilvamusic@gmail.com>
2018-05-04 20:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var instruments = ("Guitar", "Bass Guitar", "Lead Guitar", "Drums");
 var musicians = ("John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr");
 
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = []
+  for(i = 0; i < musicians)
+}
