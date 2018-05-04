b713593c0f03156af954a80112cdaa62e545d235
Derek Silva <dereksilvamusic@gmail.com>
2018-05-04 22:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-var facts = []
+var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
 
 function theBeatlesPlay(musicians, instruments) {
   var emptyArray = []
