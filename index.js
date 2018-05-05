dc640289ad7135d2b6fa3c39a5f50f1971aadde1
Derek Silva <dereksilvamusic@gmail.com>
2018-05-05 02:29:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(number){
   var loveArray = [];
-  do{newArray.push(`I love the Beatles!`)};
+  do{loveArray.push(`I love the Beatles!`)};
 }
